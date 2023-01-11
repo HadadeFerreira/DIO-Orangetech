dc689f71312887fb2fe0da6fa620c256789c0572
Hadade <hadadeferreira@gmail.com>
2023-01-10 22:00:09 -0300

    Monta linhas

--- Sintaxe/tipos-variaveis/src/App.java
+++ Sintaxe/tipos-variaveis/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int idade = 22;
+        
+        String nome = "Hadade Ferreira de Sousa ";
+
+        String meuNome = "Meu nome é: " + nome ;
+        
+        String minhaIdade = "Eu tenho " + idade + " anos.";
+
+        System.out.print(meuNome + minhaIdade);
+
     }
 }
